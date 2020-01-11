d9a466274d2f0fcda279954d251ada0e9463fed2
Peter Labadorf <plaba3.1415@gmail.com>
2020-01-11 10:39:58 -0500

    pulled over logging classes from Macerana2018

---
+++ src/main/java/frc/robot/logger/MatchDataLoggerFactory.java
@@ -0,0 +1,19 @@
+package frc.robot.logger;
+
+/**
+ * Creates DataLoggers that will send to SmartDashboard and the console. Useful
+ * for in a match.
+ * 
+ * @author dcowden
+ *
+ */
+public class MatchDataLoggerFactory extends DataLoggerFactory {
+
+	@Override
+	public DataLogger createDataLogger(String name) {
+	    // TODO: remove ConsoleDataLogger at the competition.
+		return new CompositeLogger(new SmartDashboardLogger(name));
+		// return new CompositeLogger(new SmartDashboardLogger(name), new ConsoleDataLogger(name, new WpilibTimeSource()));
+	}
+
+}
---
+++ src/main/java/frc/robot/logger/WpilibTimeSource.java
@@ -0,0 +1,28 @@
+package frc.robot.logger;
+
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * Keeps time based on the built-in wpilib Timer.
+ * 
+ * @author dcowden
+ *
+ */
+public class WpilibTimeSource implements TimeSource {
+
+	protected double startTime = Timer.getFPGATimestamp();
+
+	public void resetClock() {
+		startTime = Timer.getFPGATimestamp();
+	}
+
+	public double getElapsedSeconds() {
+		return getSystemTime() - startTime;
+	}
+
+	@Override
+	public double getSystemTime() {
+		return Timer.getFPGATimestamp();
+	}
+
+}
