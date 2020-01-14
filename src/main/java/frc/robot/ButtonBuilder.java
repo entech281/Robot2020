8a20d7349cdf4914f26bb5d4a1d87579ec3cdb5e
Peter Labadorf <plaba3.1415@gmail.com>
2020-01-13 19:58:44 -0500

    made the gang of four happy

---
+++ src/main/java/frc/robot/ButtonBuilder.java
@@ -0,0 +1,66 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.List;
+
+import java.util.ArrayList;
+
+/**
+ * @author Peter Labadorf
+ * 
+ * {@summary This class reduces boilerplate button managment code in the 
+ * operator interface implementation by building and keeping references 
+ * to all the buttons on the joystick.}
+ * 
+ */
+class JoystickButtonManager {
+    private List<JoystickButton> buttons = new ArrayList<>();
+    private Joystick joystick;
+    public JoystickButtonManager(Joystick joystick){
+        this.joystick = joystick;
+    }
+
+    public BuilderWithoutHandlers addButton(int port){
+        return new Builder(new JoystickButton(joystick, port));
+    }
+
+    public interface BuilderWithoutHandlers{
+        public Builder whenPressed(Command command);
+        public Builder whenReleased(Command command);
+        public Builder whileHeld(Command command);
+    }
+
+    public class Builder implements BuilderWithoutHandlers {
+        
+        private JoystickButton buttonBeingBuilt;
+
+        public Builder(JoystickButton button){
+            buttonBeingBuilt = button;
+        }
+        @Override
+        public Builder whenPressed(Command command) {
+            buttonBeingBuilt.whenPressed(command);
+            return this;
+        }
+
+        @Override
+        public Builder whenReleased(Command command) {
+            if(null != buttonBeingBuilt)
+                buttonBeingBuilt.whenReleased(command);
+            return this;
+        }   
+        @Override
+        public Builder whileHeld(Command command) {
+            buttonBeingBuilt.whileHeld(command);
+            return this;
+        } 
+
+        public JoystickButton add(){
+            buttons.add(buttonBeingBuilt);
+            return buttonBeingBuilt;
+        }
+    }
+}
