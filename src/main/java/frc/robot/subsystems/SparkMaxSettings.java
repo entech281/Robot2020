3d48801382c79121962a526f6047b6821e7bbc12
Aryan <aryanpn@gmail.com>
2020-01-16 20:00:47 -0500

    Added a settings object and a builder for both spark maxes and talons

--- src/main/java/frc/robot/subsystems/SparkMaxSettings.java
+++ src/main/java/frc/robot/subsystems/SparkMaxSettings.java
@@ -1,8 +1,14 @@
-package main.java.frc.robot.subsystems;
+package frc.robot.subsystems;
 
 import java.io.Serializable;
-import org.apache.commons.lang3.SerializationUtils;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANPIDController.AccelStrategy;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import org.apache.commons.lang3.SerializationUtils;
 
 
 
@@ -13,40 +19,76 @@ public class SparkMaxSettings implements Serializable{
 	public static final int PROFILE_SLOT = 0;
 	public static final int DEFAULT_FAST_FRAMERATE_MILLIS = 10;
 	public static final double DEFAULT_DEMAND = 0;
+	public Gains gains = new Gains();
+
 
 	public CurrentLimits currentLimits = new CurrentLimits();
 	public MotorRampUp rampUp = new MotorRampUp();
 	public MotorDirections motorDirections = new MotorDirections();
 	public MotorOutputLimits outputLimits = new MotorOutputLimits();
-	public NeutralMode brakeMode = NeutralMode.Brake;
-	public FeedbackDevice feedbackDevice = FeedbackDevice.QuadEncoder;
+	public IdleMode brakeMode = IdleMode.kBrake;
+	public MotionProfile profile = new MotionProfile();
+	public ControlType ctrlType = ControlType.kDutyCycle;
+	public double demand = DEFAULT_DEMAND;
+	public boolean follow = false;
+
+	public CANPIDController pidController;
+	
 
     public SparkMaxSettings copy(){
-        return SerializationUtils.clone(this);
-    }
+		return SerializationUtils.clone(this);
+	}
+	
     public void configureSparkMax(CANSparkMax spark){
 		//Current Limits
+		pidController = new CANPIDController(spark);
+
 		spark.setSmartCurrentLimit(currentLimits.smartLimit);
 		spark.setClosedLoopRampRate(rampUp.rampUpSecondsClosedLoop);
 		spark.setOpenLoopRampRate(rampUp.rampUpSecondsOpenLoop);
 		spark.setInverted(motorDirections.inverted);
-		spark.setOutputRange(outputLimits.minMotorOutput, outputLimits.maxMotorOutput);
-		spark.setIdleMode(this.brakeMode);
+		spark.setIdleMode(brakeMode);
 
-    }
+		
+		pidController.setOutputRange(outputLimits.minMotorOutput, outputLimits.maxMotorOutput);
+		pidController.setFF(gains.f);
+		pidController.setI(gains.i);
+		pidController.setP(gains.p);
+		pidController.setD(gains.d);
+		pidController.setSmartMotionAccelStrategy(profile.trapezoStrategy, PID_SLOT);
+		pidController.setSmartMotionAllowedClosedLoopError(profile.allowableClosedLoopError, PID_SLOT);
 
-    	/**
+
+		spark.burnFlash();
+	}
+
+	/**
 	 * Sets just the mode. Typically used when you're swtiching back and forth
 	 * between modes
 	 * 
 	 * @param spark
 	 */
-	public void setMode(CANSparkMax spark, double settingValue) {
-			spark.setReference(settingValue, PID_SLOT);
+	public void setMode(CANSparkMax spark, double value) {
+		CANSparkMax leader;
+		if(this.follow){
+			leader  = new CANSparkMax((int)value, MotorType.kBrushless);
+			spark.follow(leader);
+		} else {
+			pidController = spark.getPIDController();
+			pidController.setReference(value, this.ctrlType);
+		}
 	}
 
-	public void setMode(CANSparkMax spark){
-		
+	public void setMode(CANSparkMax spark) {
+		CANSparkMax leader;
+		if(this.follow){
+			leader  = new CANSparkMax((int)this.demand, MotorType.kBrushless);
+			spark.follow(leader);
+		}
+		else {
+			pidController = spark.getPIDController();
+			pidController.setReference(this.demand, this.ctrlType);
+		}
 	}
 
 	public static class Gains implements Serializable {
@@ -59,8 +101,8 @@ public class SparkMaxSettings implements Serializable{
 
 	public static class MotionProfile implements Serializable {
 		private static final long serialVersionUID = -9034830588710443069L;
-		public int cruiseVelocityEncoderClicksPerSecond = 3200;
-		public int accelerationEncoderClicksPerSecond2 = 1;
+		public int cruiseVelocityRPM = 3200;
+		public AccelStrategy trapezoStrategy = AccelStrategy.kTrapezoidal;
 		public int allowableClosedLoopError = 20;
 	}
 
---
+++ src/main/java/frc/robot/subsystems/SparkMaxSettingsBuilder.java
@@ -0,0 +1,215 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.ControlType;
+import com.revrobotics.SparkMax;
+import com.revrobotics.CANPIDController.AccelStrategy;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import frc.robot.subsystems.SparkMaxSettings.MotorOutputLimits;
+import frc.robot.subsystems.SparkMaxSettings.MotorRampUp;
+
+
+
+
+
+public class SparkMaxSettingsBuilder {
+    public static SafetySettings defaults() {
+        return new Builder(new SparkMaxSettings());
+    }
+    public static SafetySettings copyOf(SparkMaxSettings settings){
+        return new Builder(settings);
+    }
+    public static SparkMaxSettings copy(SparkMaxSettings settings){
+        return settings.copy();
+    }
+    public static SparkMaxSettings withDirectionSettings(SparkMaxSettings settings, boolean invertedDirections){
+        SparkMaxSettings s = settings.copy();
+        s.motorDirections.inverted = invertedDirections;
+        return s;
+    }
+    public static SparkMaxSettings inverted(SparkMaxSettings settings){
+        SparkMaxSettings s = settings.copy();
+        s.motorDirections.inverted = !settings.motorDirections.inverted;
+        return s;
+    }
+
+    public static SparkMaxSettings follow(SparkMaxSettings other, int otherSparkId) {
+        SparkMaxSettings s = other.copy();
+        s.follow = true;
+        s.demand = otherSparkId;
+        return s;
+    }
+
+    public static SparkMaxSettings disabledCopy(SparkMaxSettings other){
+        SparkMaxSettings s = other.copy();
+        s.ctrlType = ControlType.kDutyCycle;
+        return s;
+    }
+
+    public interface SparkControlMode {
+        public PositionControlSettings.GainSettings usePositionControl();
+
+        public SpeedControlSettings useSpeedControl();
+    }
+
+    public interface PositionControlSettings {
+        public interface GainSettings {
+            ProfileSettings withGains(double f, double p, double i, double d);
+        }
+
+        public interface ProfileSettings {
+            ProfileSettings withMotionProfile(int cruiseVelocityRPM, AccelStrategy accelStrategy, int allowableError);
+        }
+
+        public interface Finish {
+            public SparkMaxSettings build();
+        }
+    }
+
+    public interface SpeedControlSettings {
+        public SparkMaxSettings build();
+    }
+
+    public interface SafetySettings {
+        public interface BrakeMode {
+            public DirectionSettings brakeInNeutral();
+
+            public DirectionSettings coastInNeutral();
+        }
+
+        public BrakeMode withPrettySafeCurrentLimits();
+
+        public BrakeMode withCurrentLimits(int smartLimit);
+    }
+
+    public interface DirectionSettings {
+        public MotorOutputLimits defaultDirectionSettings();
+
+        public MotorOutputLimits withDirections(boolean sensorPhase, boolean inverted);
+    }
+
+    public interface MotorOutputLimits {
+        public MotorRamping noMotorOutputLimits();
+        public MotorRamping limitMotorOutputs(double peak, double min);
+    }
+
+
+    public interface MotorRamping {
+        public SparkControlMode noMotorStartupRamping();
+        public SparkControlMode withMotorRampUpOnStart(double secondsToFullPower);
+    }
+    public static class Builder 
+                implements SparkControlMode, PositionControlSettings, PositionControlSettings.GainSettings,
+                PositionControlSettings.ProfileSettings, PositionControlSettings.Finish, SpeedControlSettings,
+                SafetySettings, SafetySettings.BrakeMode, DirectionSettings, MotorOutputLimits, MotorRamping{
+
+
+        private SparkMaxSettings settings = new SparkMaxSettings();
+
+        private Builder(SparkMaxSettings settings){
+            this.settings = settings;
+        }
+
+        @Override
+        public SparkControlMode noMotorStartupRamping() {
+            settings.rampUp.rampUpSecondsClosedLoop = 0;
+            settings.rampUp.rampUpSecondsOpenLoop = 0;
+            return this;
+        }
+
+        @Override
+        public SparkControlMode withMotorRampUpOnStart(double secondsToFullPower) {
+            settings.rampUp.rampUpSecondsOpenLoop = secondsToFullPower;
+            return this;
+        }
+
+        @Override
+        public MotorRamping noMotorOutputLimits() {
+            settings.outputLimits.maxMotorOutput = 1;
+            settings.outputLimits.minMotorOutput = -1;
+            return this;
+        }
+
+        @Override
+        public MotorRamping limitMotorOutputs(double peak, double min) {
+            settings.outputLimits.maxMotorOutput = peak;
+            settings.outputLimits.minMotorOutput = min;
+            return this;
+        }
+
+        @Override
+        public MotorOutputLimits defaultDirectionSettings() {
+            settings.motorDirections.inverted = false;
+            settings.motorDirections.sensorPhase = false;
+            return this;
+        }
+
+        @Override
+        public MotorOutputLimits withDirections(boolean sensorPhase, boolean inverted) {
+            settings.motorDirections.sensorPhase = sensorPhase;
+            settings.motorDirections.inverted = inverted;
+            return this;
+        }
+
+        @Override
+        public DirectionSettings brakeInNeutral() {
+            settings.brakeMode = IdleMode.kBrake;
+            return this;
+        }
+
+        @Override
+        public DirectionSettings coastInNeutral() {
+            settings.brakeMode = IdleMode.kCoast;
+            return this;
+        }
+
+        @Override
+        public BrakeMode withPrettySafeCurrentLimits() {
+            settings.currentLimits.smartLimit = 5;
+            return this;
+        }
+
+        @Override
+        public BrakeMode withCurrentLimits(int smartLimit) {
+            settings.currentLimits.smartLimit = smartLimit;
+            return this;
+        }
+
+        @Override
+        public SparkMaxSettings build() {
+            // TODO Auto-generated method stub
+            return settings;
+        }
+
+        @Override
+        public ProfileSettings withMotionProfile(int cruiseVelocityRPM, AccelStrategy accelStrategy,
+                int allowableError) {
+            settings.profile.allowableClosedLoopError = allowableError;
+            settings.profile.cruiseVelocityRPM = cruiseVelocityRPM;
+            settings.profile.trapezoStrategy = accelStrategy;
+            return this;
+        }
+
+        @Override
+        public ProfileSettings withGains(double f, double p, double i, double d) {
+            settings.gains.f = f;
+            settings.gains.p = p;
+            settings.gains.i = i;
+            settings.gains.d = d;
+            return this;
+        }
+
+        @Override
+        public GainSettings usePositionControl() {
+            settings.ctrlType = ControlType.kSmartMotion;
+            return this;
+        }
+
+        @Override
+        public SpeedControlSettings useSpeedControl() {
+            settings.ctrlType = ControlType.kVelocity;
+            return this;
+        }
+
+    }
+}
