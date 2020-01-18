b1f950be3fd0840dbb55c2bb948a9c250d9f621f
Peter Labadorf <plaba3.1415@gmail.com>
2020-01-18 11:01:57 -0500

    fixed drive

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,30 +1,40 @@
 package frc.robot.subsystems;
 
-
-
- import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import frc.robot.RobotMap;
+import frc.robot.controllers.TalonSpeedController;
 
 
 
  public class IntakeSubsystem extends BaseSubsystem{
     private final double INTAKE_SPEED = 1;
 
-    private final TalonSRX intakeMotor = new TalonSRX(RobotMap.CAN.INTAKE_MOTOR);
+    private final WPI_TalonSRX intakeMotor = new WPI_TalonSRX(RobotMap.CAN.INTAKE_MOTOR);
+    private TalonSpeedController intakeMotorController;
 
     @Override
     public void initialize() {
+        TalonSettings motorSettings = TalonSettingsBuilder.defaults()
+            .withCurrentLimits(20, 15, 200)
+            .brakeInNeutral()
+            .withDirections(false, false)
+            .noMotorOutputLimits()
+            .noMotorStartupRamping()
+            .useSpeedControl()
+            .build();
+        intakeMotorController = new TalonSpeedController(intakeMotor, motorSettings);
+        intakeMotorController.configure();
         intakeMotor.set(ControlMode.PercentOutput,0);
     }
 
     public void startIntakeMotor() {
-        intakeMotor.set(ControlMode.PercentOutput,INTAKE_SPEED);
+        intakeMotorController.setDesiredSpeed(INTAKE_SPEED);
     }
 
     public void stopIntakeMotor(){
-        intakeMotor.set(ControlMode.PercentOutput,0);
+        intakeMotorController.setDesiredSpeed(0);
     }
 
  } 
