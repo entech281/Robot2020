12b0071ffb51bbd20cbae1932f5904a982226033
Peter Labadorf <plaba3.1415@gmail.com>
2020-01-18 10:51:02 -0500

    changed to talon

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,28 +1,30 @@
 package frc.robot.subsystems;
 
- import com.revrobotics.CANSparkMax;
- import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
- import frc.robot.RobotMap;
+
+ import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import frc.robot.RobotMap;
 
 
 
  public class IntakeSubsystem extends BaseSubsystem{
     private final double INTAKE_SPEED = 1;
 
-    private final CANSparkMax intakeMotor = new CANSparkMax(RobotMap.CAN.INTAKE_MOTOR, MotorType.kBrushless);
+    private final TalonSRX intakeMotor = new TalonSRX(RobotMap.CAN.INTAKE_MOTOR);
 
     @Override
     public void initialize() {
-        intakeMotor.set(0);
+        intakeMotor.set(ControlMode.PercentOutput,0);
     }
 
     public void startIntakeMotor() {
-        intakeMotor.set(INTAKE_SPEED);
+        intakeMotor.set(ControlMode.PercentOutput,INTAKE_SPEED);
     }
 
     public void stopIntakeMotor(){
-        intakeMotor.set(0);
+        intakeMotor.set(ControlMode.PercentOutput,0);
     }
 
  } 
