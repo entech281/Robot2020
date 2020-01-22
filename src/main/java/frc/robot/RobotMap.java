28bd75d5028ebf61802bc4a3773f3fc578059101
Peter Labadorf <plaba3.1415@gmail.com>
2020-01-21 19:51:57 -0500

    stuff

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -18,6 +18,16 @@ public class RobotMap{
         //public static final int solenoid2 = 1;
     }
 
+    public interface AUTO_DRIVE_CONSTANTS {
+        //TODO DO NOT RUN WITH THESE CONSTANTS!!! find real constants using charecterization.
+        public static final double ksVolts = 0.22;
+        public static final double kvVoltSecondsPerMeter = 1.98;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+    
+        // Example value only - as above, this must be tuned for your drive!
+        public static final double kPDriveVel = 8.5;
+    }
+
     public interface GAMEPAD{
         public static final int driverStick = 0;
     }
