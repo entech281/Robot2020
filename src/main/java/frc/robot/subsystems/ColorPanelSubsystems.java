2f878d0f5074484ccae4c96c490b4f7a9429caaf
LenovoG550 <rohitgan2004@gmail.com>
2020-01-25 16:30:25 -0500

    fixed again

--- src/main/java/frc/robot/subsystems/ColorPanelSubsystems.java
+++ src/main/java/frc/robot/subsystems/ColorPanelSubsystems.java
@@ -9,18 +9,18 @@ import edu.wpi.first.wpilibj.util.Color;
 
 public class ColorPanelSubsystems extends BaseSubsystem{
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
-    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-    private final ColorMatch m_colorMatcher = new ColorMatch();
+    private final ColorSensorV3  colorSensor = new ColorSensorV3(i2cPort);
+    private final ColorMatch  colorMatcher = new ColorMatch();
     private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
     private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
     private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
     private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
     @Override
     public void initialize() {
-        m_colorMatcher.addColorMatch(kBlueTarget);
-        m_colorMatcher.addColorMatch(kGreenTarget);
-        m_colorMatcher.addColorMatch(kRedTarget);
-        m_colorMatcher.addColorMatch(kYellowTarget);  
+         colorMatcher.addColorMatch(kBlueTarget);
+         colorMatcher.addColorMatch(kGreenTarget);
+         colorMatcher.addColorMatch(kRedTarget);
+         colorMatcher.addColorMatch(kYellowTarget);  
     }
     public void detectColor(){
         /**
@@ -33,13 +33,13 @@ public class ColorPanelSubsystems extends BaseSubsystem{
          * an object is the more light from the surroundings will bleed into the 
          * measurements and make it difficult to accurately determine its color.
          */
-        Color detectedColor = m_colorSensor.getColor();
+        Color detectedColor =  colorSensor.getColor();
 
         /**
          * Run the color match algorithm on our detected color
          */
         String colorString;
-        ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+        ColorMatchResult match =  colorMatcher.matchClosestColor(detectedColor);
 
         if (match.color == kBlueTarget) {
             colorString = "Blue";
