4be8e0b8467aa06c6b88759c0f5cf24db14bf84b
Aryan <aryanpn@gmail.com>
2020-02-11 20:13:12 -0500

    WIP

---
+++ src/main/java/frc/robot/commands/climb/BaseClimbCommand.java
@@ -0,0 +1,10 @@
+package frc.robot.commands.climb;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClimbSubsystem;
+
+public class BaseClimbCommand extends CommandBase{
+    
+    private final ClimbSubsystem climb;
+    
+}
