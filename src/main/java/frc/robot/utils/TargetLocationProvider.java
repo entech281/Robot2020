3e346e8f1046774be741b97f2216a950157b3c30
Peter Labadorf <plaba3.1415@gmail.com>
2020-03-03 18:39:30 -0500

    fixed formatting

--- src/main/java/frc/robot/utils/TargetLocationProvider.java
+++ src/main/java/frc/robot/utils/TargetLocationProvider.java
@@ -13,6 +13,45 @@ public class TargetLocationProvider{
      * of the calculated angle to the camera range angle, or
      *    theta_calculated / theta_camera = pix_detected / pix_height
      * pix_detected is the input to the class.
-     * theta_calculated depends on the height of the target, the distance that the targe is away from the 
+     * theta_calculated depends on the height of the target, the height difference 
+     * between the camera and the bottom of the target, and the distance the camera 
+     * is away from the target.
+     * 
+     * Here is the formula to calculate the angle
+     * theta_calculated = atan( ( h_diff + t_height) / dist_away ) - atan( h_diff / dist_away )
+     * 
+     * Great! We have a formula that depends on the distance away and some other constants and 
+     * is tied to the height of the target in pixels. All we need to do is invert the function 
+     * used to calculate theta_calculated, and grab dist_away from it. 
+     * 
+     * There's a problem though,
+     * That function is not invertable. It has two branches which are. So there are two answers
+     * to what the distance away actually is, that result from the same image. 
+     * 
+     * I got the functions from Mathematica.
     */
+
+    private static double branchOne(double h,double d,double k){
+        return Complex.I.multiply(4 * 1 / Math.tan(k))
+            .subtract(4)
+            .reciprocal()
+            .multiply(
+                Complex.I.multiply(
+                    Complex.I.multiply(2 * k).exp().add(1)
+                )
+                .multiply(2 * d + h)
+                .add(
+                    Complex.I.multiply(
+                        Complex.I.multiply(- 2 * k).exp()
+                            .multiply(8 * d * d + 8 * d * h + h * h * (1 + Math.cos(2 * k)) )
+                            .getArgument()
+                    )
+                    .multiply(1/2)
+                    .exp()
+                    .multiply(Math.sqrt(2))
+                    .multiply(Math.pow(8 * d * d + 8 * d * h + h * h * (1 + Math.cos(2 * k) ), 1 / 4))
+                )
+            ).getReal()
+            * 1 / Math.pow(Math.cos(k), 2);   
+    }
 }
