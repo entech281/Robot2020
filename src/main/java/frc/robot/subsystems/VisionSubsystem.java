5514a5194ea1301eca3dccb114bc60c06f921037
Peter Labadorf <plaba3.1415@gmail.com>
2020-03-06 18:12:54 -0500

    need to checkout master

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -11,7 +11,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotConstants;
 import frc.robot.pose.*;
 import frc.robot.utils.JStruct;
-import frc.robot.utils.VisionDataProcessor;
+import frc.robot.vision.OpenMV;
+import frc.robot.vision.VisionDataProcessor;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.SerialPort.Port;
 
 import java.io.FileOutputStream;
 import java.io.StringReader;
@@ -25,17 +28,20 @@ import java.util.*;
  */
 public class VisionSubsystem extends BaseSubsystem {
 
+    private Timer timer = new Timer();
+
     private static final int BAUD_RATE = 115200;
     private static final int TIMEOUT = 2;
     private static final String VISION_SCRIPT_LOCATION = Filesystem.getDeployDirectory() + "python/vision.py";
-    private static final int FRAME_RATE = 20;
-    
+    private static final int WAIT_TO_DEPLOY_SCRIPT_SECONDS = 4; 
+    private static final int FRAME_RATE_FPS = 20;
+
     private OpenMV openMV;
 
     private VisionData visionData = VisionData.DEFAULT_VISION_DATA;
     private VisionDataProcessor processor;
 
-    private State state = State.OPENMV_NOT_CONNECTED;
+    private OpenMVState state = OpenMVState.NOT_CONNECTED;
 
     @Override
     public void initialize() {
@@ -48,12 +54,27 @@ public class VisionSubsystem extends BaseSubsystem {
     public void customPeriodic(RobotPose rPose, FieldPose fPose) {
         logger.driverinfo("Vision Status", state);
 
-        switch(state){
-            case OPENMV_NOT_CONNECTED:
-                break; // We could try reconnecting just in case we get vision halfway through the match
-            case OPENMV_CONNECTED:
-
-
+        switch (state) {
+            case NOT_CONNECTED:
+                break; // We could try reconnecting just in case we get vision halfway through the
+                       // match
+            case CONNECTED:
+                try {
+                    openMV.stopScript();
+                    openMV.enableFb(1);
+                    state = OpenMVState.STOPPED_SCRIPT;
+                } catch (Exception e) {
+                    state = OpenMVState.ERROR;
+                }
+            case STOPPED_SCRIPT:
+                if(timer.hasPeriodPassed(WAIT_TO_DEPLOY_SCRIPT_SECONDS)){
+                    
+                }
+                break;
+            case SCRIPT_RUNNING:
+                break;
+            case ERROR:
+                break;
         }
 
         /*if(isConnected){
@@ -76,122 +97,28 @@ public class VisionSubsystem extends BaseSubsystem {
     }
 
     public void ensureConnected(){
-        if(state == State.OPENMV_NOT_CONNECTED){
+        if(state == OpenMVState.NOT_CONNECTED){
             try{
-                openMV = new OpenMV(BAUD_RATE, TIMEOUT);
-                logger.driverinfo("Vision connection initialization", "SUCCESS");
-                state = State.OPENMV_CONNECTED;
+                openMV = new OpenMV(BAUD_RATE, Port.kUSB1, TIMEOUT);
+                state = OpenMVState.CONNECTED;
             } catch(Exception e){
-                logger.driverinfo("Vision connection initialization", "FAILED");
+                state = OpenMVState.NOT_CONNECTED;
             }
         }
     }
 
     public boolean isConnected(){
-        return state != State.OPENMV_NOT_CONNECTED;
+        return state != OpenMVState.NOT_CONNECTED;
     }
 
-    private enum State{
-        OPENMV_NOT_CONNECTED,
-        OPENMV_CONNECTED,
+    private enum OpenMVState{
+        NOT_CONNECTED,
+        CONNECTED,
+        ERROR,
         STOPPED_SCRIPT,
-        STARTED_SCRIPT,
         SCRIPT_RUNNING
     }
 
-    private class OpenMV{
-        private JStruct struct = new JStruct();
-        private SerialPort connection;
-
-        private static final int FB_HDR_SIZE   = 12;
-
-        //USB Debug commands
-        private static final byte USBDBG_CMD            = (byte) 48  ;
-        private static final byte USBDBG_FRAME_SIZE     = (byte) 0x81;
-        private static final byte USBDBG_FRAME_DUMP     = (byte) 0x82;
-        private static final byte USBDBG_SCRIPT_EXEC    = (byte) 0x05;
-        private static final byte USBDBG_SCRIPT_STOP    = (byte) 0x06;
-        private static final byte USBDBG_SCRIPT_RUNNING = (byte) 0x87;
-        private static final byte USBDBG_SYS_RESET      = (byte) 0x0C;
-        private static final byte USBDBG_FB_ENABLE      = (byte) 0x0D;
-        private static final byte USBDBG_TX_BUF_LEN     = (byte) 0x8E;
-        private static final byte USBDBG_TX_BUF         = (byte) 0x8F;
-        
-        public OpenMV(int baudRate, int timeout) {
-            connection = new SerialPort(baudRate, SerialPort.Port.kUSB1);
-        }
-
-        private void write(byte[] bytes) {
-            System.out.println(bytes);
-            connection.write(bytes, bytes.length);
-        }
-
-        private byte[] read(int numBytes) {
-            return connection.read( numBytes );
-        }
-
-        public long[] fbSize() throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD, USBDBG_FRAME_SIZE, FB_HDR_SIZE));
-            var buffer = read(12);
-            return struct.unpack("III", buffer);
-        }
-
-        public void execScript(byte[] buff) throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD,USBDBG_SCRIPT_EXEC, buff.length));
-            write(buff);
-        }
-
-        public void stopScript() throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD,USBDBG_SCRIPT_STOP,0));
-        }
-
-        public boolean scriptRunning() throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD, USBDBG_SCRIPT_RUNNING, 4));
-            return struct.unpack("I", read(4))[0] != 0;
-        }
-
-        public void reset() throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD, USBDBG_SYS_RESET, 0));
-        }
-
-        public int txBufLen() throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD, USBDBG_TX_BUF_LEN, 4));
-            return (int)struct.unpack("I", read(4))[0];
-        }
-
-        public byte[] txBuf(int numBytes) throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD, USBDBG_TX_BUF, numBytes));
-            return read(numBytes);
-        }
-
-        public void enableFb(int enable) throws Exception {
-            write(struct.pack("<BBI", USBDBG_CMD, USBDBG_FB_ENABLE, 4));
-            write(struct.pack("<I", enable));
-        }
-
-        public byte[] getSerialOutput() throws Exception {
-            return txBuf(txBufLen());
-        }
-
-        public byte[] fbDump() throws Exception {
-            long[] size = fbSize();
-            
-            if (size[0] == 0){
-                throw new Exception();
-            }
-            long numBytes = 0;
-            if (size[2] > 2){ //JPEG
-                System.out.println("Here");
-                numBytes = size[2];
-            }
-            else
-                numBytes = size[0]*size[1]*size[2];
-            // read fb data
-            write(struct.pack("<BBI", (long)USBDBG_CMD, (long)USBDBG_FRAME_DUMP, numBytes));
-            byte[] buff = read((int)numBytes);
-            return buff;
-        }
-
-    }
+    
 
 }
---
+++ src/main/java/frc/robot/vision/OpenMV.java
@@ -0,0 +1,127 @@
+package frc.robot.vision;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import frc.robot.utils.JStruct;
+
+public class OpenMV {
+    private JStruct struct = new JStruct();
+    private SerialPort connection;
+
+    private static int FB_HDR_SIZE = 12;
+
+    // USB Debug commands
+    private static byte USBDBG_CMD = (byte) 48;
+    private static byte USBDBG_FW_VERSION = (byte) 0x80;
+    private static byte USBDBG_FRAME_SIZE = (byte) 0x81;
+    private static byte USBDBG_FRAME_DUMP = (byte) 0x82;
+    private static byte USBDBG_ARCH_STR = (byte) 0x83;
+    private static byte USBDBG_SCRIPT_EXEC = (byte) 0x05;
+    private static byte USBDBG_SCRIPT_STOP = (byte) 0x06;
+    private static byte USBDBG_SCRIPT_SAVE = (byte) 0x07;
+    private static byte USBDBG_SCRIPT_RUNNING = (byte) 0x87;
+    private static byte USBDBG_TEMPLATE_SAVE = (byte) 0x08;
+    private static byte USBDBG_DESCRIPTOR_SAVE = (byte) 0x09;
+    private static byte USBDBG_ATTR_READ = (byte) 0x8A;
+    private static byte USBDBG_ATTR_WRITE = (byte) 0x0B;
+    private static byte USBDBG_SYS_RESET = (byte) 0x0C;
+    private static byte USBDBG_FB_ENABLE = (byte) 0x0D;
+    private static byte USBDBG_TX_BUF_LEN = (byte) 0x8E;
+    private static byte USBDBG_TX_BUF = (byte) 0x8F;
+
+    private static int ATTR_CONTRAST = (byte) 0;
+    private static int ATTR_BRIGHTNESS = (byte) 1;
+    private static int ATTR_SATURATION = (byte) 2;
+    private static int ATTR_GAINCEILING = (byte) 3;
+
+    private static long BOOTLDR_START = 0xABCD0001;
+    private static long BOOTLDR_RESET = 0xABCD0002;
+    private static long BOOTLDR_ERASE = 0xABCD0004;
+    private static long BOOTLDR_WRITE = 0xABCD0008;
+
+    public OpenMV(int baudRate,Port port, int timeout) {
+        connection = new SerialPort(baudRate, port);
+        connection.setTimeout(timeout);
+        //System.out.println(connection.isOpen());
+    }
+
+    private void write(byte[] bytes) {
+        connection.write(bytes, bytes.length);
+    }
+
+    private byte[] read(int numBytes) {
+        return  connection.read(numBytes);
+    }
+
+    public long[] fbSize() throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD, USBDBG_FRAME_SIZE, FB_HDR_SIZE));
+        var buffer = read(12);
+        return struct.unpack("III", buffer);
+    }
+
+    public void execScript(byte[] buff) throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD,USBDBG_SCRIPT_EXEC, buff.length));
+        write(buff);
+    }
+
+    public void stopScript() throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD,USBDBG_SCRIPT_STOP,0));
+    }
+
+    public boolean scriptRunning() throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD, USBDBG_SCRIPT_RUNNING, 4));
+        return struct.unpack("I", read(4))[0] != 0;
+    }
+
+    public void reset() throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD, USBDBG_SYS_RESET, 0));
+    }
+
+    public boolean bootloaderStart() throws Exception {
+        write(struct.pack("<I", BOOTLDR_START));
+        return struct.unpack("I", read(4))[0] == BOOTLDR_START;
+    }
+
+    public void bootloaderReset() throws Exception {
+        write(struct.pack("<I", BOOTLDR_RESET));
+    }
+
+    public void flashErase(byte sector) throws Exception {
+        write(struct.pack("<II", BOOTLDR_ERASE, sector));
+    }
+
+    public int txBufLen() throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD, USBDBG_TX_BUF_LEN, 4));
+        return (int)struct.unpack("I", read(4))[0];
+    }
+
+    public byte[] txBuf(int i) throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD, USBDBG_TX_BUF, i));
+        return read(i);
+    }
+
+    public long[] fwVersion() throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD, USBDBG_FW_VERSION, 12));
+        return struct.unpack("III", read(12));
+    }
+
+    public void enableFb(int enable) throws Exception {
+        write(struct.pack("<BBI", USBDBG_CMD, USBDBG_FB_ENABLE, 4));
+        write(struct.pack("<I", enable));
+    }
+
+    public byte[] fbDump(String s) throws Exception {
+        long[] size = fbSize();
+        if (size[0] == 0){
+            return new byte[0];
+        }
+        System.out.println("Here!");
+        long numBytes = 0;
+        if (size[2] > 2){ //JPEG
+            numBytes = size[2];
+        }
+        // read fb data
+        write(struct.pack("<BBI", (long)USBDBG_CMD, (long)USBDBG_FRAME_DUMP, numBytes));
+        return read((int)numBytes);
+    }
+}
